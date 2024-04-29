b71fb31878ff573ec78e4321d8184d8102e35ca5
RappyTV <rappytv@rappytv.com>
2024-04-29 21:44:08 +0200

    Add tab completions for perk manage sub commands

--- src/main/java/com/rappytv/perks/command/PerkCommand.java
+++ src/main/java/com/rappytv/perks/command/PerkCommand.java
@@ -1,11 +1,15 @@
 package com.rappytv.perks.command;
 
 import com.rappytv.perks.PerkPlugin;
+import com.rappytv.perks.config.PlayerData;
+import com.rappytv.perks.perks.Perk;
 import com.rappytv.perks.util.Util;
 import com.rappytv.rylib.RyLib;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PerkCommand extends com.rappytv.rylib.util.Command<PerkPlugin> {
@@ -21,7 +25,7 @@ public class PerkCommand extends com.rappytv.rylib.util.Command<PerkPlugin> {
             return;
         }
 
-        if(!player.hasPermission("perks.command")) {
+        if(!player.hasPermission("perks.menu")) {
             player.sendMessage(RyLib.get().i18n().translate("noPermission"));
             return;
         }
@@ -29,8 +33,42 @@ public class PerkCommand extends com.rappytv.rylib.util.Command<PerkPlugin> {
         Util.openPerkGUI(plugin, player, 0);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public List<String> complete(CommandSender sender, String prefix, String[] args) {
+        List<String> list = new ArrayList<>();
+        if(args.length == 1) {
+            if(sender.hasPermission("perks.manage.add")) list.add("add");
+            if(sender.hasPermission("perks.manage.remove")) {
+                list.add("remove");
+                list.add("clear");
+            }
+            return tab(args[0], list);
+        } else if(args.length == 2) {
+            boolean add = sender.hasPermission("perks.manage.add") && args[0].equalsIgnoreCase("add");
+            boolean remove = sender.hasPermission("perks.manage.remove") && args[0].equalsIgnoreCase("remove");
+            boolean clear = sender.hasPermission("perks.manage.remove") && args[0].equalsIgnoreCase("clear");
+
+            if(add || remove || clear) return players;
+        } else if(args.length == 3) {
+            boolean add = sender.hasPermission("perks.manage.add") && args[0].equalsIgnoreCase("add");
+            boolean remove = sender.hasPermission("perks.manage.remove") && args[0].equalsIgnoreCase("remove");
+            PlayerData data = PlayerData.get(Bukkit.getOfflinePlayer(args[1]).getUniqueId());
+            if(data == null) return null;
+
+            if(add) {
+                for(Perk perk : Perk.perks) {
+                    if(!data.getActivePerks().contains(perk.getId()))
+                        list.add(perk.getId());
+                }
+            } else if(remove) {
+                for(Perk perk : Perk.perks) {
+                    if(data.getActivePerks().contains(perk.getId()))
+                        list.add(perk.getId());
+                }
+            }
+            return tab(args[2], list);
+        }
         return null;
     }
 }
